0774ec168fa1a68ff20aa7c6de6339c5c60d2299
JonathanVarghese24 <jonathanvarghese24@gmail.com>
2024-05-25 11:36:34 -0500

    Initial Commit

---
+++ AI Tag Scanner/AI_Tag_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Tag_ScannerApp.swift
+//  AI Tag Scanner
+//
+//  Created by JV on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Tag_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
